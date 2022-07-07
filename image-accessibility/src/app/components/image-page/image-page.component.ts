8fa82ff22724f839687d551e249c1b1a8fa8498e
dan933 <69749413+dan933@users.noreply.github.com>
2022-07-07 18:21:06 +1000

    returning image url's as a string

--- image-accessibility/src/app/components/image-page/image-page.component.ts
+++ image-accessibility/src/app/components/image-page/image-page.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core'
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 // Firebase imports
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+//import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage'
+import { listAll } from 'firebase/storage';
 
 import { Observable } from 'rxjs';
 
-export interface Car {
-  Make:string
-}
+// export interface Car {
+//   Make:string
+// }
 
 
 @Component({
@@ -17,9 +20,12 @@ export interface Car {
 })
 export class ImagePageComponent implements OnInit {
 
-  cars$!: Observable<Car[]>;
+  //cars$!: Observable<Car[]>;
+
+  images: string[] = [];
 
   constructor(
+    private storage: AngularFireStorage
     //firestore: Firestore
   ) {
     //const data:any = collection(firestore, 'cars');
@@ -27,9 +33,30 @@ export class ImagePageComponent implements OnInit {
 
   }
 
+  //This is where you store the file names and download url's
+filelist:any = []
+
+//This is the function you call (put it in ngOnInit or something of the like) to get the filenames
+getFileList() {
+
+  const storageRef = this.storage.ref('images');
+
+  storageRef.listAll()
+    .subscribe({
+      next: (resp) => {
+        resp.items.forEach((imageRef) => {
+          imageRef.getDownloadURL().then((url) => {
+            this.images.push(url)
+          }).finally(() => {console.log(this.images)})
+        })
+      }
+  })
+}
+
   speech_voices: any;
 
   ngOnInit(): void {
+    this.getFileList();
     var speech_voices;
     if ('speechSynthesis' in window) {
       speech_voices = window.speechSynthesis.getVoices();
