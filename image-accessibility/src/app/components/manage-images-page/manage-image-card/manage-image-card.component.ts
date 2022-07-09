bc07f4d1b852382da6f829ce31714806cdd2e01e
dan933 <69749413+dan933@users.noreply.github.com>
2022-07-09 13:04:20 +1000

    passed image data to manage image card

--- image-accessibility/src/app/components/manage-images-page/manage-image-card/manage-image-card.component.ts
+++ image-accessibility/src/app/components/manage-images-page/manage-image-card/manage-image-card.component.ts
@@ -1,11 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface Image{
+  id: string,
+  url:string,
+  caption: string
+}
 
 @Component({
   selector: 'app-manage-image-card',
   templateUrl: './manage-image-card.component.html',
   styleUrls: ['./manage-image-card.component.scss']
 })
+
+
 export class ManageImageCardComponent implements OnInit {
+
+  @Input() image: Image = {id:'', caption:'', url:''};
 
   constructor() { }
 
--- image-accessibility/src/app/components/manage-images-page/manage-images-page.component.ts
+++ image-accessibility/src/app/components/manage-images-page/manage-images-page.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Image {
+  id:string,
+  url: string,
+  caption:string
+}
 
 @Component({
   selector: 'app-manage-images-page',
@@ -7,9 +16,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageImagesPageComponent implements OnInit {
 
-  constructor() { }
+  images$!: Observable<Image[]>;
+  images!: Image[];
+
+  constructor(
+    //private storage: AngularFireStorage,
+    private firestore: Firestore
+  ) {
+    const data: any = collection(this.firestore, 'Images');
+    this.images$ = collectionData(data, {idField:"id"});
+   }
 
   ngOnInit(): void {
+    this.images$.subscribe((resp) =>  {this.images = resp, console.log(resp)})
   }
 
 }
